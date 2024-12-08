678ca8bc89c212ad8aa6df66a6b8b1aa57e2f3f3
TMaiza <tmaiza@uc.cl>
2024-12-08 01:13:42 -0300

    re

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,6 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.assets.precompile += %w( application.css application.js )
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
@@ -61,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "Tarea_Kapso_production"
+  # config.active_job.queue_name_prefix = "tarea_kapso_production"
 
   config.action_mailer.perform_caching = false
 
